2c930a72b9e255ad64313037238023de2c369da1
webdev.sp <webdev.sp@amtexsystems.com>
2023-05-17 15:29:06 +0530

    added google tag manager

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'G-KHMV08ENG6'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 // Find all widget divs
 const WidgetDivs = document.querySelectorAll('.reddit_widget')
